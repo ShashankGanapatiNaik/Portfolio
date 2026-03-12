0ee554b148e6c729a8fb48a113d1efe552165200
ShashankGanapatiNaik <shashanknaik6226@gmail.com>
2026-03-12 21:36:24 +0530

    vercel-edit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // IMPORTANT for Vercel deployment
   server: {
     proxy: {
       "/api": {
